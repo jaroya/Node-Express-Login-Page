1b635d7f6a1a7736022736f28529bf1a0fadfa97
jaroya <michael.jaroya@gmail.com>
2020-05-17 08:05:50 +0300

    Cookies Complete

--- login.js
+++ login.js
@@ -1,25 +1,48 @@
 const path = require('path');
 const express = require('express');
 const helmet = require('helmet');
+const cookieman = require('cookie-parser');
 const app = express();
 
 app.use(helmet());
 app.use(express.static('public'));
 app.use(express.urlencoded());
 app.use(express.json());
+app.use(cookieman());
+
 
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use((req,res,next)=>{
+
+	if(req.query.msg == "fail"){
+		res.locals.msg = "Login Failed!! Try again";
+
+	}else if(req.query.msg == "success"){
+
+		res.locals.msg = "Login successful"
+	}else{
+
+		res.locals.msg = ``
+	}
+
+	next();
+})
+
 app.get('/',(req,res,next)=>{
 	res.render('index',{
-		user:res.cookies.user
+		username:req.cookies.user
 	});
 });
 
 app.get('/login',(req,res,next)=>{
+
+	console.log(req.query);
 	res.render('login');
+
+
 });
 
 app.post('/process_login', (req,res,next)=>{
@@ -31,10 +54,20 @@ app.post('/process_login', (req,res,next)=>{
 		res.cookie('user',email);
 		res.redirect('/?msg=success');
 	}else{
+
 		res.redirect('/login?msg=fail')
 	}
 	//res.json(req.body);
 });
+
+app.get('/logout',(req,res,next)=>{
+	res.clearCookie('user');
+	res.redirect('/login');
+});
+
+app.get('/story/:storyId',(req,res,next)=>{
+	res.send(`The message url is: ${req.params.storyId}->${req.params.post}`)
+})
 
 
 
